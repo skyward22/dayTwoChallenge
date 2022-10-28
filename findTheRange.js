fc5bce41c45529458d506984fc8af4ea86bad7fa
Skyler Trent <skyler.trent93@gmail.com>
2022-10-27 19:35:11 -0600

    dayTwoChallenge

---
+++ findTheRange.js
@@ -0,0 +1,7 @@
+let array = [28, 43, -12, 30, 4, 0, -36];
+
+const smallestNumber = Math.min(...array);
+const largestNumber = Math.max(...array);
+
+console.log("Smallest Value:", smallestNumber);
+console.log("Largest Value:", largestNumber);
